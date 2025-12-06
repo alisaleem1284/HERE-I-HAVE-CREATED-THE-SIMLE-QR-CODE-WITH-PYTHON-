3bba047b4222db7152990c5b86ed754d56d4311a
M.ALI SALEEM <1semasterx@gmail.com>
2025-12-06 07:50:36 +0500

    QR.PY
    HERE I AHVE CREATED THE SIMPLE QR CODE WITH PYTHON

--- work.py
+++ work.py
@@ -1,12 +1,13 @@
 import qrcode 
 
 url = input("enter the url:").strip()
-
-file_path = "C:\\Users\\Muhammad.Ali\Desktop\\r language\\qrcode.png"
+# HERE IN THE FILE PATH YOU HAVE TO PUT THE PATH OF THE FLODER WHERE YOU NEED TO STORE THE QR CODE
+file_path = "qrcode.png"
 
 qr = qrcode.QRCode()
 qr.add_data(url)
 
 img = qr.make_image()
 img.save(file_path )
+
 print("the qr code is created")
